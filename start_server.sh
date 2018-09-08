91255d149225d5c8b9651b1f56820db0948b30b9
JunaidRahim <junaidrahim8d@gmail.com>
2018-09-08 20:23:47 +0530

    ngrok instructions

--- start_server.sh
+++ start_server.sh
@@ -3,5 +3,8 @@
 # This script runs ngrok and starts the python server in the background
 # just one file to start things off
 
+# make sure you have ngrok authtoken configured so that your hosting runs doesnt run for a limited time
+# Instructions are in the README
+
 ./ngrok http 3000 &
 python3 main.py
